740c49c6e08506f1f908c5f676bddc0a1ea8c241
Aamir-shakil <aamirshakil21@gmail.com>
2024-10-28 15:08:11 +0000

    Feat: Types js for typing effect

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/** code for showing and hiding navbar**/
+// code for showing and hiding navbar
 var togglebtn=document.querySelector(".togglebtn");
 var nav=document.querySelector(".nav-bar");
 var links=document.querySelector(".nav-bar li");
@@ -7,3 +7,11 @@ togglebtn.addEventListener("click", function(){
     this.classList.toggle("click");
     nav.classList.toggle("open");
 });
+
+// code implementing typing text effect
+var typed=new Typed(".input",{
+    strings:["Software Devloper", "Manchester Metropolitan Student", "Retail Associate"],
+    typeSpeed:70,
+    backSpeed:55,
+    loop:true
+})
